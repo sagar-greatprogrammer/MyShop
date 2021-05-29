0304cff970b0685a54d26aacb81656c742643175
acer <acer@DESKTOP-S186E54>
2021-05-29 14:54:02 +0530

    Added Order Creation

---
+++ MyShop.Core/Contracts/IOrderService.cs
@@ -0,0 +1,18 @@
+﻿using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Contracts
+{
+    public interface IOrderService
+    {
+        void CreateOrder(Order baseOrder, List<BasketItemViewModel> basketItems);
+        List<Order> GetOrderList();
+        Order GetOrder(string Id);
+        void UpdateOrder(Order updatedOrder);
+    }
+}
--- MyShop.WebUI/Controllers/BasketController.cs
+++ MyShop.WebUI/Controllers/BasketController.cs
@@ -1,4 +1,5 @@
 ﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,13 +10,17 @@ namespace MyShop.WebUI.Controllers
 {
     public class BasketController : Controller
     {
+        IRepository<Customer> customers;
         IBasketService basketService;
+        IOrderService orderService;
 
-        public BasketController(IBasketService BasketService)
+        public BasketController(IBasketService BasketService, IOrderService OrderService, IRepository<Customer> Customers)
         {
             this.basketService = BasketService;
-                    }
-        // GET: Basket
+            this.orderService = OrderService;
+            this.customers = Customers;
+        }
+        // GET: Basket2
         public ActionResult Index()
         {
             var model = basketService.GetBasketItems(this.HttpContext);
@@ -41,6 +46,57 @@ namespace MyShop.WebUI.Controllers
             var basketSummary = basketService.GetBasketSummary(this.HttpContext);
 
             return PartialView(basketSummary);
+        }
+
+        [Authorize]
+        public ActionResult Checkout()
+        {
+            Customer customer = customers.Collection().FirstOrDefault(c => c.Email == User.Identity.Name);
+
+            if (customer != null)
+            {
+                Order order = new Order()
+                {
+                    Email = customer.Email,
+                    City = customer.City,
+                    State = customer.State,
+                    Street = customer.Street,
+                    FirstName = customer.FirstName,
+                    Surname = customer.LastName,
+                    ZipCode = customer.ZipCode
+                };
+
+                return View(order);
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+
+        }
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult Checkout(Order order)
+        {
+
+            var basketItems = basketService.GetBasketItems(this.HttpContext);
+            order.OrderStatus = "Order Created";
+            order.Email = User.Identity.Name;
+
+            //process payment
+
+            order.OrderStatus = "Payment Processed";
+            orderService.CreateOrder(order, basketItems);
+            basketService.ClearBasket(this.HttpContext);
+
+            return RedirectToAction("Thankyou", new { OrderId = order.Id });
+        }
+
+        public ActionResult ThankYou(string OrderId)
+        {
+            ViewBag.OrderId = OrderId;
+            return View();
         }
     }
 }
